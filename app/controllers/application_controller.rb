b95105663d4449840ccb178406760483b36bc1e1
Dillon Welch <daw0328@gmail.com>
2014-11-19 12:06:20 -0800

    Add empty defs for route/stop I18n.t

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  #Schedule table
-  ###I18n.t values for direction names
-  ###I18n.t values for stop names
 end
