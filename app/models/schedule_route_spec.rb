a7f4b370f3d8c13796be452cfd6e1c2afe68562f
Dillon Welch <daw0328@gmail.com>
2014-11-19 16:16:24 -0800

    Remove unnecessary folders.

--- app/models/schedule_route_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe ScheduleRoute do
-  ['schedule', 'route'].each do |field|
-    describe "The #{field} field" do
-      it_behaves_like("an association field", :schedule_route, field)
-    end
-  end
-end
