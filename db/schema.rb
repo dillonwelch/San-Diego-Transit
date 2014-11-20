49585f9ffbf988cd67ba770e19d1b6a6ef97a8d3
Dillon Welch <daw0328@gmail.com>
2014-11-19 16:22:15 -0800

    Switch to using postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141118222304) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "routes", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
